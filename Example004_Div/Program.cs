ccd13a162c68665b068caa07711e3bd77c78fe0b
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-09 18:18:53 +0300

    Example004_1

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberOne = 10;
+double numberTwo = 20;
+Console.WriteLine(numberOne / numberTwo);
