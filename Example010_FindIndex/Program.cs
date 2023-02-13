194eac2619993626e19a36e9989650937241af23
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-13 22:47:56 +0300

    Example010

---
+++ Example010_FindIndex/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = { 1, 12, 18, 4, 51, 61, 17, 18 };
+int findIndex = 18;
+
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i] == findIndex)
+  {
+    Console.WriteLine(i);
+    break;
+  }
+}
