aa0f7e12eb23121123afeb5d894b29f0971b0e56
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-13 23:35:33 +0300

    Add Example011

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] array = new int[10];
+
+FillArray(array);
+ReadArray(array);
+Console.WriteLine();
+Console.WriteLine(FindIndex(array, 6));
+
+int FindIndex(int[] array, int value)
+{
+  int index = -1;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] == value)
+    {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
+
+void FillArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = new Random().Next(1, 10);
+  }
+}
+
+void ReadArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write(array[i] + " ");
+  }
+}
+
