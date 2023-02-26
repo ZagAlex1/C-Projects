b5a46ea4c08a217171cf673ece380dc52ff61a47
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-26 20:04:04 +0300

    Add task Example015

---
+++ Example015_Fibanacci/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int Fibonacci(int n)
+{
+  if (n == 1 || n == 2)
+    return 1;
+  else
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 20; i++)
+{
+  System.Console.WriteLine($"F({i}) = {Fibonacci(i)}");
+}
+
