3c9a2cbb1aed0bb1cbfd968bcad80780ba8e1233
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-26 18:59:17 +0300

    Add task Example013

---
+++ Example013_TwoSqears/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Clear();
+
+int[,] pic = new int[,]
+{
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0},
+    {0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+    {0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+    {0,0,1,0,0,1,1,1,1,1,1,0,0,1,0,0},
+    {0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0},
+    {0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0},
+    {0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0},
+    {0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0},
+    {0,0,1,0,0,1,1,1,1,1,1,0,0,1,0,0},
+    {0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+    {0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+    {0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+};
+
+void PrintImage(int[,] image)
+{
+  for (int rows = 0; rows < image.GetLength(0); rows++)
+  {
+    for (int columns = 0; columns < image.GetLength(1); columns++)
+    {
+      if (image[rows, columns] == 0)
+        System.Console.Write($" ");
+      else
+        System.Console.Write($"+");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+void FillImage(int row, int col)
+{
+  if (pic[row, col] == 0)
+  {
+    pic[row, col] = 1;
+    FillImage(row - 1, col);
+    FillImage(row, col - 1);
+    FillImage(row + 1, col);
+    FillImage(row, col + 1);
+  }
+}
+
+PrintImage(pic);
+FillImage(6,4);
+PrintImage(pic);
